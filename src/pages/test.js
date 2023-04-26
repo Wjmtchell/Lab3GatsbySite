aba0a1dde42eb5ea9f5e12d94e8863390191ed8e
shaylondon <112662764+shaylondon@users.noreply.github.com>
2023-04-26 18:10:43 -0500

    hopefully made home page work with buttons

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,263 +1,359 @@
-// import React from 'react';
-// import { StyleSheet, css } from 'aphrodite';
-// import Headroom from 'react-headroom';
-// import Header3 from 'components/Header3';
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import cn from 'classnames';
 
-// function renderSection1(props) {
-//   return (
-//     <section className={css(section1Styles.section1)}>
-//       <div className={css(section1Styles.flexCol)}>
-//         <div className={css(section1Styles.rect)} />
-//         <div className={css(section1Styles.flexCol__cell)}>
-//           <h1 className={css(section1Styles.hero_title2)}>Team 1 Portfolio</h1>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+function renderSection1(props) {
+  return (
+    <section className={css(section1Styles.section1)}>
+      <px-grid track-style="flex-grow: 1;" x="0px 1440fr 0px" y="0px 175fr 0fr">
+        <div className={css(section1Styles.flexCol)}>
+          <article className={css(section1Styles.content_box)} />
+          <div className={css(section1Styles.flexCol__cell)}>
+            <h1 className={css(section1Styles.hero_title2)}>Team 1 Portfolio</h1>
+          </div>
+        </div>
+      </px-grid>
+    </section>
+  );
+}
 
-// function renderSection2(props) {
-//   return (
-//     <section className={css(section2Styles.section2)}>
-//       <div className={css(section2Styles.flexCol)}>
-//         <div className={css(section2Styles.flexCol__cell)}>
-//           <div className={css(section2Styles.rect2)} />
-//         </div>
-//         <div className={css(section2Styles.flexCol__cell1)}>
-//           <h1 className={css(section2Styles.hero_title1)}>About Us</h1>
-//         </div>
-//         <div className={css(section2Styles.flexCol__cell2)}>
-//           <div className={css(section2Styles.rect3)} />
-//         </div>
-//         <div className={css(section2Styles.flexCol__cell3)}>
-//           <div className={css(section2Styles.rect1)} />
-//         </div>
-//         <div className={css(section2Styles.flexCol__cell4)}>
-//           <h1 className={css(section2Styles.hero_title)}>Mitchell</h1>
-//         </div>
-//         <div className={css(section2Styles.flexCol__cell5)}>
-//           <h1 className={css(section2Styles.hero_title2)}>Shay</h1>
-//         </div>
-//         <div className={css(section2Styles.rect11)} />
-//         <div className={css(section2Styles.flexCol__cell6)}>
-//           <h1 className={css(section2Styles.hero_title3)}>William</h1>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
+function renderSection2(props) {
+  return (
+    <section className={css(section2Styles.section2)}>
+      <div className={css(section2Styles.flexCol)}>
+        <button id={'abtn'} className={css(section2Styles.btn)} onClick={props.aboutButtonClicked}>
+          <span className={css(section2Styles.btn__text)}>About Us</span>
+        </button>
 
-// export default function Lab03Home(props) {
-//   return (
-//     <React.Fragment>
-//       <Headroom tag="header" className="page-header">
-//         <nav className="max:show">
-//           <Header3 />
-//         </nav>
-//       </Headroom>
+        <button id={'mbtn'} className={css(section2Styles.btn1)} onClick={props.sButtonClicked}>
+          <span className={css(section2Styles.btn__text1)}>Shay</span>
+        </button>
 
-//       <main className={cn(css(styles.main), 'lab03home')}>
-//         <div className={css(styles.main__cell)}>{renderSection1(props)}</div>
-//         <div className={css(styles.main__cell)}>{renderSection2(props)}</div>
-//       </main>
-//     </React.Fragment>
-//   );
-// }
+        <button id={'mbtn'} className={css(section2Styles.btn2)} onClick={props.sButtonClicked}>
+          <span className={css(section2Styles.btn__text2)}>Mitchell</span>
+        </button>
 
-// const section1Styles = StyleSheet.create({
-//   section1: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     flexGrow: 1,
-//     minHeight: 175
-//   },
-//   flexCol: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     flexGrow: 1
-//   },
-//   rect: {
-//     backgroundColor: 'rgb(79,236,161)',
-//     outline: '6px solid rgb(70,70,70)',
-//     outlineOffset: -6,
-//     position: 'relative',
-//     minHeight: 140
-//   },
-//   flexCol__cell: {
-//     marginTop: -140,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative'
-//   },
-//   hero_title2: {
-//     font: '400 128px/1.36 "Nunito Sans", Helvetica, Arial, serif',
-//     color: 'rgb(70,70,70)',
-//     letterSpacing: '0px',
-//     width: '65.97%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '0px auto'
-//   }
-// });
+        <button className={css(section2Styles.btn3)} onClick={props.wButtonClicked}>
+          {/* wbtn */}
+          <span className={css(section2Styles.btn__text3)}>William</span>
+        </button>
+      </div>
+    </section>
+  );
+}
 
-// const section2Styles = StyleSheet.create({
-//   section2: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     flexGrow: 1,
-//     minHeight: 849
-//   },
-//   flexCol: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '166px 364px 203px'
-//   },
-//   flexCol__cell: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 1
-//   },
-//   rect2: {
-//     backgroundColor: 'rgb(79,236,161)',
-//     borderRadius: '10px 10px 10px 10px',
-//     outline: '3px solid rgb(70,70,70)',
-//     width: '85.67%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     minHeight: 72,
-//     margin: '0px auto'
-//   },
-//   flexCol__cell1: {
-//     marginTop: -72,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 5
-//   },
-//   hero_title1: {
-//     font: '400 64px/1.35 "Nunito Sans", Helvetica, Arial, serif',
-//     color: 'rgb(255,255,255)',
-//     letterSpacing: '0px',
-//     width: '38.2%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '0px auto'
-//   },
-//   flexCol__cell2: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 2
-//   },
-//   rect3: {
-//     backgroundColor: 'rgb(79,236,161)',
-//     borderRadius: '10px 10px 10px 10px',
-//     outline: '2px solid rgb(70,70,70)',
-//     outlineOffset: -2,
-//     width: '30.9%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     minHeight: 100,
-//     margin: '93px 0% 0px 69.1%'
-//   },
-//   flexCol__cell3: {
-//     marginTop: -100,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 3
-//   },
-//   rect1: {
-//     backgroundColor: 'rgb(79,236,161)',
-//     borderRadius: '10px 10px 10px 10px',
-//     outline: '3px solid rgb(70,70,70)',
-//     outlineOffset: -3,
-//     width: '30.9%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     minHeight: 100,
-//     margin: '0px 69.1% 0px 0%'
-//   },
-//   flexCol__cell4: {
-//     marginTop: -78,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 4
-//   },
-//   hero_title: {
-//     font: '400 48px/1.16 "Roboto", Helvetica, Arial, serif',
-//     color: 'rgb(70,70,70)',
-//     letterSpacing: '0px',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '0px 25px'
-//   },
-//   flexCol__cell5: {
-//     marginTop: -56,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 7
-//   },
-//   hero_title2: {
-//     display: 'flex',
-//     justifyContent: 'flex-end',
-//     font: '400 48px/1.16 "Roboto", Helvetica, Arial, serif',
-//     color: 'rgb(70,70,70)',
-//     textAlign: 'right',
-//     letterSpacing: '0px',
-//     width: '83.71%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '0px auto'
-//   },
-//   rect11: {
-//     backgroundColor: 'rgb(79,236,161)',
-//     borderRadius: '10px 10px 10px 10px',
-//     outline: '3px solid rgb(70,70,70)',
-//     outlineOffset: -3,
-//     width: '30.9%',
-//     position: 'relative',
-//     minHeight: 100,
-//     margin: '122px auto 0px'
-//   },
-//   flexCol__cell6: {
-//     marginTop: -78,
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative',
-//     zIndex: 6
-//   },
-//   hero_title3: {
-//     font: '400 48px/1.16 "Roboto", Helvetica, Arial, serif',
-//     color: 'rgb(70,70,70)',
-//     letterSpacing: '0px',
-//     width: '22.19%',
-//     position: 'relative',
-//     flexGrow: 1,
-//     margin: '0px auto'
-//   }
-// });
+export default function Lab03Home(props) {
+  return (
+    <React.Fragment>
 
-// const styles = StyleSheet.create({
-//   main: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     backgroundColor: 'rgb(255,255,255)',
-//     padding: 'undefinedpx undefinedpx undefinedpx undefinedpx',
-//     position: 'relative',
-//     overflow: 'hidden'
-//   },
-//   main__cell: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     position: 'relative'
-//   }
-// });
+      <main className={cn(css(styles.main), 'lab03home')}>
+        <div className={css(styles.main__cell)}>{renderSection1(props)}</div>
+        <div className={css(styles.main__cell)}>{renderSection2(props)}</div>
+      </main>
+    </React.Fragment>
+  );
+}
+
+const section1Styles = StyleSheet.create({
+  section1: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    flexGrow: 1
+  },
+  flexCol: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%'
+  },
+  content_box: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(79,236,161)',
+    borderStyle: 'none',
+    borderWidth: '0px',
+    outline: '6px solid rgb(0,0,0)',
+    outlineOffset: -6,
+    width: '100%',
+    height: 175,
+    position: 'relative',
+    margin: '0px 0px'
+  },
+  flexCol__cell: {
+    marginTop: -175,
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    flex: '1 1 auto'
+  },
+  hero_title2: {
+    '@media (min-width: 1920px) and (max-width: 2999px)': {
+      textAlign: 'center',
+      justifyContent: 'center'
+    },
+    '@media (max-width: 1919px)': {
+      textAlign: 'center',
+      justifyContent: 'center'
+    },
+    '@media (max-width: 1199px)': {
+      fontSize: '108px'
+    },
+    '@media (max-width: 991px)': {
+      fontSize: '92px'
+    },
+    '@media (max-width: 767px)': {
+      fontSize: '72px'
+    },
+    '@media (max-width: 575px)': {
+      fontSize: '64px'
+    },
+    '@media (max-width: 479px)': {
+      fontSize: '56px'
+    },
+    '@media (max-width: 383px)': {
+      fontSize: '52px'
+    },
+    display: 'flex',
+    font: '400 128px/1.36 "Nunito Sans", Helvetica, Arial, serif',
+    color: 'rgb(70,70,70)',
+    letterSpacing: '0px',
+    width: 950,
+    height: 175,
+    position: 'relative',
+    minWidth: 950,
+    margin: '0px auto'
+  }
+});
+
+const section2Styles = StyleSheet.create({
+  section2: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    flexGrow: 1
+  },
+  flexCol: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    position: 'relative',
+    flexGrow: 1,
+    margin: '166px 364px 203px'
+  },
+  btn: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(79,236,161)',
+    borderRadius: '10px 10px 10px 10px',
+    outline: '3px solid rgb(70,70,70)',
+    outlineOffset: -3,
+    width: 600,
+    height: 100,
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    transitionProperty: 'transform',
+    ':hover': {
+      transform: 'scale(1.1)'
+    },
+    position: 'relative',
+    flex: '1 0 auto',
+    minHeight: 0,
+    minWidth: 600,
+    margin: '0px 0px'
+  },
+  btn__text: {
+    '@media (max-width: 1199px)': {
+      fontSize: '40px'
+    },
+    '@media (max-width: 991px)': {
+      fontSize: '36px'
+    },
+    '@media (max-width: 767px)': {
+      fontSize: '32px'
+    },
+    '@media (max-width: 575px)': {
+      fontSize: '28px'
+    },
+    '@media (max-width: 479px)': {
+      fontSize: '24px'
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    font: '400 64px/1.16 "Nunito Sans", Helvetica, Arial, serif',
+    color: 'rgb(255,255,255)',
+    textAlign: 'center',
+    letterSpacing: '0px',
+    width: '100%',
+    position: 'relative',
+    flexGrow: 1,
+    margin: '22px 0px'
+  },
+  btn1: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(79,236,161)',
+    borderRadius: '10px 10px 10px 10px',
+    outline: '3px solid rgb(70,70,70)',
+    outlineOffset: -3,
+    width: 220,
+    height: 100,
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    transitionProperty: 'transform',
+    ':hover': {
+      transform: 'scale(1.1)'
+    },
+    position: 'relative',
+    flex: '1 0 auto',
+    minHeight: 0,
+    minWidth: 220,
+    margin: '50px auto'
+  },
+  btn__text1: {
+    '@media (max-width: 1199px)': {
+      fontSize: '40px'
+    },
+    '@media (max-width: 991px)': {
+      fontSize: '36px'
+    },
+    '@media (max-width: 767px)': {
+      fontSize: '32px'
+    },
+    '@media (max-width: 575px)': {
+      fontSize: '28px'
+    },
+    '@media (max-width: 479px)': {
+      fontSize: '24px'
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    font: '400 48px/1.16 "Roboto", Helvetica, Arial, serif',
+    color: 'rgb(70,70,70)',
+    textAlign: 'center',
+    letterSpacing: '0px',
+    width: '100%',
+    height: 'auto',
+    aspectRatio: '1.86',
+    position: 'relative',
+    margin: '22px 0px'
+  },
+  btn2: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(79,236,161)',
+    borderRadius: '10px 10px 10px 10px',
+    outline: '3px solid rgb(70,70,70)',
+    outlineOffset: -3,
+    width: 220,
+    height: 100,
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    transitionProperty: 'transform',
+    ':hover': {
+      transform: 'scale(1.1)'
+    },
+    position: 'relative',
+    flex: '1 0 auto',
+    minHeight: 0,
+    minWidth: 220,
+    margin: '0px auto'
+  },
+  btn__text2: {
+    '@media (max-width: 1199px)': {
+      fontSize: '40px'
+    },
+    '@media (max-width: 991px)': {
+      fontSize: '36px'
+    },
+    '@media (max-width: 767px)': {
+      fontSize: '32px'
+    },
+    '@media (max-width: 575px)': {
+      fontSize: '28px'
+    },
+    '@media (max-width: 479px)': {
+      fontSize: '24px'
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    font: '48px/1.16 "Roboto", Helvetica, Arial, serif',
+    color: 'rgb(70,70,70)',
+    textAlign: 'center',
+    letterSpacing: '0px',
+    width: '100%',
+    height: 'auto',
+    aspectRatio: '1.86',
+    position: 'relative',
+    margin: '22px 0px'
+  },
+  btn3: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(79,236,161)',
+    borderRadius: '10px 10px 10px 10px',
+    outline: '3px solid rgb(70,70,70)',
+    outlineOffset: -3,
+    width: 220,
+    height: 100,
+    cursor: 'pointer',
+    transitionDuration: '0.3s',
+    transitionProperty: 'transform',
+    ':hover': {
+      transform: 'scale(1.1)'
+    },
+    position: 'relative',
+    flex: '1 0 auto',
+    minHeight: 0,
+    minWidth: 220,
+    margin: '50px auto 0px'
+  },
+  btn__text3: {
+    '@media (max-width: 1199px)': {
+      fontSize: '40px'
+    },
+    '@media (max-width: 991px)': {
+      fontSize: '36px'
+    },
+    '@media (max-width: 767px)': {
+      fontSize: '32px'
+    },
+    '@media (max-width: 575px)': {
+      fontSize: '28px'
+    },
+    '@media (max-width: 479px)': {
+      fontSize: '24px'
+    },
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    font: '400 48px/1.16 "Roboto", Helvetica, Arial, serif',
+    color: 'rgb(70,70,70)',
+    textAlign: 'center',
+    letterSpacing: '0px',
+    width: '100%',
+    height: 'auto',
+    aspectRatio: '2.82',
+    position: 'relative',
+    margin: '22px 0px'
+  }
+});
+
+const styles = StyleSheet.create({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'rgb(255,255,255)',
+    position: 'relative',
+    overflow: 'hidden'
+  },
+  main__cell: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative'
+  }
+});
