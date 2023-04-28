77ed2f165987736d39a607d90d84e3c182aa23bc
Mitch <m32kurt@gmail.com>
2023-04-28 07:13:05 -0500

    changed about us format

--- src/pages/about.js
+++ src/pages/about.js
@@ -61,7 +61,7 @@ function renderSection2(props) {
         <div className={css(section2Styles.rect3)}>
           <div className={css(section2Styles.txt1)}>
           <div style={{textAlign: 'center', fontSize: '30px', textDecoration: 'underline', textUnderlinePosition: 'under', fontWeight: 'bold'}}> Shay </div>
-            I'm an Electrical Engineering student from Iowa City, IA. I spend almost all of my free time machining. My hobbies include dirt biking and playing fetch with my two dogs every mroning and evening. Click my page to learn more!
+            I'm an Electrical Engineering student from Iowa City, IA. My hobbies include dirt biking and playing fetch with my two dogs every morning and evening. I spend almost all of my free time machining. Click my page to learn more!
             <br></br>
             <br></br>
             <b>Email:</b> mitchell-kurt@uiowa.edu
