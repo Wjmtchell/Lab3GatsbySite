9184ae97fbafb482e8a0eb411cb547111ab92161
shaylondon <112662764+shaylondon@users.noreply.github.com>
2023-04-28 11:21:08 -0500

    fixed home path

--- src/pages/william.js
+++ src/pages/william.js
@@ -20,7 +20,7 @@ function renderSection2(props) {
           <div
             style={{ '--src': `url(${home})` }}
             className={css(section2Styles.content_box1)}
-            onClick={() => {navigate("/home")}}
+            onClick={() => {navigate("/index")}}
           />
 
           <button className={css(section2Styles.btn)} onClick={() => {navigate("/about")}}>
