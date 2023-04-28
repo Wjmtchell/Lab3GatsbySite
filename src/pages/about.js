83d2fe75c8264f4b6ca40bdc45cc7dca7e7d4f18
William Mitchell <william-j-mitchell@uiowa.edu>
2023-04-28 12:45:37 -0500

    Fixed Mitch Image

--- src/pages/about.js
+++ src/pages/about.js
@@ -59,7 +59,7 @@ function renderSection2(props) {
         <div className={css(section2Styles.flexRow__spacer)} />
 
         <div className={css(section2Styles.rect3)}>
-          <div className={css(section2Styles.txt1)}>
+          <div className={css(section2Styles.txt)}>
           <div style={{textAlign: 'center', fontSize: '30px', textDecoration: 'underline', textUnderlinePosition: 'under', fontWeight: 'bold'}}> Mitch </div>
             I'm an Electrical Engineering student from Iowa City, IA. My hobbies include dirt biking and playing fetch with my two dogs every morning and evening. I spend almost all of my free time machining. Check out <a href="/mitchell">my page</a> to learn more.
             <br></br>
