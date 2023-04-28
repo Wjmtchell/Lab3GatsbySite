e00a5df68f617ee6c18bff68a619343e384212bc
Mitch <m32kurt@gmail.com>
2023-04-28 06:59:53 -0500

    changed identifier

--- src/pages/shay.js
+++ src/pages/shay.js
@@ -50,8 +50,8 @@ function renderSection2(props) {
       </div>
       <div className={css(section2Styles.comment_styling)}>
       <Disqus
-        identifier="williamPage"
-        title="William"
+        identifier="shayPage"
+        title="Shay"
         />
       </div>
     </section>
