02712041e0d9196c153cbbdd95ecfcc7eb7a1b2f
William Mitchell <william-j-mitchell@uiowa.edu>
2023-04-27 19:52:22 -0500

    Comment fully implemented on William page

--- src/pages/william.js
+++ src/pages/william.js
@@ -47,11 +47,12 @@ function renderSection2(props) {
         stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea
         as soon as I can. There is nothing surprising in this. If they but knew it, almost all men in their degree, some
         time or other, cherish very nearly the same feelings towards the ocean with me.
-      </div>
-      <Disqus
+        <Disqus
         identifier="williamPage"
         title="William"
         />
+      </div>
+
     </section>
 
   );
