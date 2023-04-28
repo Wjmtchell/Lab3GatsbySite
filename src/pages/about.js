318b528d906cbde525194950ff151cf394cc8796
Mitch <m32kurt@gmail.com>
2023-04-28 12:24:35 -0500

    added link on about

--- src/pages/about.js
+++ src/pages/about.js
@@ -59,9 +59,9 @@ function renderSection2(props) {
         <div className={css(section2Styles.flexRow__spacer)} />
 
         <div className={css(section2Styles.rect3)}>
-          <div className={css(section2Styles.txt)}>
+          <div className={css(section2Styles.txt1)}>
           <div style={{textAlign: 'center', fontSize: '30px', textDecoration: 'underline', textUnderlinePosition: 'under', fontWeight: 'bold'}}> Mitch </div>
-            I'm an Electrical Engineering student from Iowa City, IA. My hobbies include dirt biking and playing fetch with my two dogs every morning and evening. I spend almost all of my free time machining. Click my page to learn more!
+            I'm an Electrical Engineering student from Iowa City, IA. My hobbies include dirt biking and playing fetch with my two dogs every morning and evening. I spend almost all of my free time machining. Click my Check out <a href="/mitchell">my page</a> to learn more.
             <br></br>
             <br></br>
             <b>Email:</b> mitchell-kurt@uiowa.edu
