32daa5e9d83070c1fdf3ed55c5ef9b8747335f06
wjmtchell <william-j-mitchell@uiowa.edu>
2023-09-26 12:55:30 -0500

    Login Page

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/login', (req,res)=> res.render("pages/login"))
+  .post('/login/auth', async (req,res)=>{
+    const {username, password} = req.body;
+    try {
+      const values = [username,password];
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users WHERE username = $1 AND password=$2', values)
+      if (result.rows.length===1){
+        res.send('Logged In!');
+      } else {
+        res.send('Login failed. Please make sure you have entered the correct username and password');
+      }
+    } catch (error) {
+      console.error('Login error', error);
+      res.status(500).send('Database Connection Failed')
+    }
+  })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
