ca5fe2a84bc25ba440542eb0c7eee3a7b510c049
William Mitchell <william-j-mitchell@uiowa.edu>
2023-10-02 17:36:37 -0500

    Login Functionality

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const express = require('express')
-const path = require('path')
-const { Pool } = require('pg')
-const PORT = process.env.PORT || 5001
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5001;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
-})
+});
 
 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({extended:false}))
+  .use(session({secret:'Hello World', resave:false,saveUninitialized:false}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  //.get('/login', (req,res)=> res.render("pages/login"))
+  .get('/login', (req,res)=> res.render("pages/login"))
+  .post('/login/auth', async (req,res)=>{
+    const {username, password} = req.body;
+    try {
+      const values = [username,password];
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users WHERE username = $1 AND password= $2', values)
+      if (result.rows.length===1){
+        req.session.user = username;
+        //res.send('Logged In!');
+        res.redirect('/?message=Login%20Successful');
+      } else {
+       //res.send('Login failed. Please make sure you have entered the correct username and password');
+        res.redirect('/login?message=Login Failed')
+      }
+    } catch (error) {
+      console.error('Login error', error);
+      res.status(500).send('Database Connection Failed')
+    }
+  })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
