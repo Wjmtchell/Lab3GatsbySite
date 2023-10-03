113cac32ae25974c0219535fce92dae3d8310012
William Mitchell <william-j-mitchell@uiowa.edu>
2023-10-03 12:09:54 -0500

    Admin Page add functionality

--- index.js
+++ index.js
@@ -20,28 +20,37 @@ express()
   .use(session({secret:'Hello World', resave:false,saveUninitialized:false}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/login', (req,res)=> res.render("pages/login"))
+  .get('/', (req, res) => {
+    const message = req.query.message || '';
+    res.render('pages/index', {message});})
+  .get('/login', (req,res)=> {
+    const message = req.query.message || '';
+    res.render("pages/login", {message});})
   .post('/login/auth', async (req,res)=>{
     const {username, password} = req.body;
     try {
       const values = [username,password];
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM users WHERE username = $1 AND password= $2', values)
+      const result = await client.query('SELECT * FROM users WHERE username = $1 AND password= $2', values);
       if (result.rows.length===1){
         req.session.user = username;
-        //res.send('Logged In!');
-        res.redirect('/?message=Login%20Successful');
+
+        if(result.rows[0].type ==1){ 
+          res.redirect('/admin?message=Login%20Successful.%20Welcome%20Admin');
+        } else{ 
+          res.redirect('/?message=Login%20Successful.%20Welcome.');
+        }
       } else {
        //res.send('Login failed. Please make sure you have entered the correct username and password');
-        res.redirect('/login?message=Login Failed')
+        res.redirect('/login?message=Login Failed');
       }
     } catch (error) {
       console.error('Login error', error);
-      res.status(500).send('Database Connection Failed')
+      res.status(500).send('Database Connection Failed');
     }
   })
-  .get('/db', async (req, res) => {
+  .get('/admin', async (req, res) => {
+
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM users');
@@ -51,6 +60,17 @@ express()
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
+    }
+  })
+  .post('/admin/add', async (req,res)=>{
+    const {username,password,type}=req.body;
+    try {
+      const client = await pool.connect();
+      await client.query('INSERT INTO users (username,password,type) VALUES ($1,$2,$3)',[username,password,type]);
+
+      res.redirect('/admin');
+    } catch(error) {
+      res.redirect('/admin?message=Error%20Adding%20User');
     }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
