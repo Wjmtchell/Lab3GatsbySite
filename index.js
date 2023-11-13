3a2985dc9e18327661d28b76f7ad97f2aba8a8ad
Mitchell Kurt <m32kurt@gmail.com>
2023-11-13 14:49:42 -0600

    try more main menus

--- index.js
+++ index.js
@@ -55,6 +55,9 @@ express()
   .get('/test_menu', (req,res)=> {
     const message = req.query.message || '';
     res.render("pages/test_menu", {message});})
+  .get('/test_menu2', (req,res)=> {
+    const message = req.query.message || '';
+    res.render("pages/test_menu2", {message});})
   .post('/login/auth', async (req,res)=>{
     const {username, password} = req.body;
     try {
