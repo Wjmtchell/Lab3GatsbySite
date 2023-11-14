8fd2b1482c31797281b2e551418358418c45a718
root <root@pudgington>
2023-11-14 11:32:05 -0600

    added dynamic employee list functionality

--- index.js
+++ index.js
@@ -80,9 +80,19 @@ express()
     const message = req.query.message || '';
     const user = req.session.user;
     res.render("pages/student_edit", {user, message});})
-  .get('/employee_list', (req,res)=> {
+  .get('/employee_list', async (req,res)=> {
     const message = req.query.message || '';
     const user = req.session.user;
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users WHERE type = 2;');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
     res.render("pages/employee_list", {user, message});})
   .get('/employee_info', (req,res)=> {
     const message = req.query.message || '';
