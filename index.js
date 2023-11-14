3ac541903b87a92165215fb27ec44eee93ad4a55
wjmtchell <william-j-mitchell@uiowa.edu>
2023-11-14 13:55:34 -0600

    Translation support

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const session = require('express-session');
 const path = require('path');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
+const i18next = require('i18next');
+const Backend = require('i18next-locize-backend');
+const i18nextMiddleware= require('i18next-http-middleware');
 const PORT = process.env.PORT || 5001;
-
+i18next
+    .use(Backend)
+    .use(i18nextMiddleware.LanguageDetector)
+    .init({
+        lng:'en',
+        fallbackLng: 'en',
+        debug: true,
+        saveMissing: true,
+        backend:{
+            projectId: '3cc31e1c-db80-4280-a832-13111693710a',
+            apiKey: '640ced37-c6a4-49ce-8d31-4be7f1879971',
+            referenceLng: 'en',
+            version:'latest'
+        },
+    });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -19,18 +36,81 @@ function getOrdinal(n) {
 }
 
 express()
+  .use(i18nextMiddleware.handle(i18next))
+  .use((req, res, next) => {
+    // Pass i18next instance to the template
+    res.locals.i18next = i18next;
+    console.log('i18next in locals:', res.locals.i18next); // Add this line for debuggin
+    next();
+  })
   .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.urlencoded({extended:false}))
   .use(session({secret:'Hello World', resave:false,saveUninitialized:false}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/change-language', async(req, res) => {
+    const { lng } = req.query;
+    if (lng) {
+      i18next.changeLanguage(lng);
+    }
+    await i18next.reloadResources();
+    res.redirect('back'); // Redirect back to the previous page
+  })
   .get('/', (req, res) => {
     const message = req.query.message || '';
     const user = req.session.user;
     res.render('pages/index', {user, message});})
   .get('/login', (req,res)=> {
     const message = req.query.message || '';
-    res.render("pages/login", {message});})
+    const user = req.session.user;
+    res.render("pages/login", {user, message});})
+  .get('/main_menu', (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    res.render("pages/main_menu", {user, message});})
+  .get('/student_list', async (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users WHERE type = 3');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/student_list', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }})
+  .get('/student_info', (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    res.render("pages/student_info", {user, message});})
+  .get('/student_edit', (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    res.render("pages/student_edit", {user, message});})
+  .get('/employee_list', async (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM users WHERE type = 2');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/employee_list', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+    })
+  .get('/employee_info', (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    res.render("pages/employee_info", {user,message});})
+  .get('/employee_edit', (req,res)=> {
+    const message = req.query.message || '';
+    const user = req.session.user;
+    res.render("pages/employee_edit", {user,message});})
   .post('/login/auth', async (req,res)=>{
     const {username, password} = req.body;
     try {
@@ -60,7 +140,7 @@ express()
     }
   })
   .get('/admin', async (req, res) => {
-    // const user = req.session.user;
+    const user = req.session.user;
     if(req.session.type != 1){
       res.redirect('/?message=You%20are%20not%20authorized%20to%20access%20that%20page.')
     }else{
