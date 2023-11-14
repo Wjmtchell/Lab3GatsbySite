b6cf82bebb872e5eafdcc5f47e6fa3688c1237bd
shaylondon <shay-london@uiowa.edu>
2023-11-14 12:55:44 -0600

    modified params

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ express()
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM users WHERE type = 2;');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/employee_list', results);
+      res.render('pages/db', results);
       client.release();
     } catch (err) {
       console.error(err);
