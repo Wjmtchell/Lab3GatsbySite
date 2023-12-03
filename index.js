d1906a0369bba20349a45449a3b6e9199c5f0f0e
William Mitchell <william-j-mitchell@uiowa.edu>
2023-12-03 15:25:49 -0600

    Testing admin page

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ express()
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM users');
         const results = { 'results': (result) ? result.rows : null};
-        res.render('pages/db', results);
+        res.render('pages/db', {results,user});
         client.release();
       } catch (err) {
         console.error(err);
